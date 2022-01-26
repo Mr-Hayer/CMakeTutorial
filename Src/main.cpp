5ea0cda9920b28d3169db6efee645aae77a5137f
hsingh <dareborn.13@gmail.com>
2022-01-26 22:55:46 +0100

    CMake| Tested gitsubmodule glfw

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,6 +5,7 @@
 #include "dbgUtils.h"
 
 #include "services_interfaces.h"
+#include <glfw/glfw3.h>
 
 int main()
 {
@@ -21,5 +22,32 @@ int main()
 	ipservice.ServiceInfoR();
 	#endif
 
+	GLFWwindow* window;
+
+	if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+	window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+	// Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+ 	// Terminate GLFW
+    glfwTerminate();
+
 	return 0;
 }
+
+
